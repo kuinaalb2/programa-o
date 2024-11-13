178125a66f45e187e3b22c31b2213a3d97a7dc2a
kuinaalb2 <142520695+kuinaalb2@users.noreply.github.com>
2024-11-13 10:43:25 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -5,8 +5,31 @@
 //chamada
 
 criaCartao(
-    'Programação'
-     'o que é JavaScricpt'
-     'O JavaScript é uma linguagem de programação'
-
-)
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+    )
+    
+    criaCartao(
+        'Programação',
+        'O que é Python?',
+        'O Python é uma linguagem de programação'
+    )
+    
+    criaCartao(
+        'Geografia',
+        'Qual a capital da França?',
+        'A capital da França é Paris'
+    )
+    
+    criaCartao(
+        'Programação',
+        'O que é uma função?',
+        'Uma função é um bloco de código que executa alguma tarefa'
+    )
+    
+    criaCartao(
+        'Culinaria',
+        'Como faz bolo de cenoura?',
+        'Com cenoura:)'
+    )
